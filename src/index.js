6c824296bf8394f64251408fff1e565ca3ca6087
Florentino Navarro <ceacademy.web@gmail.com>
2023-01-05 23:44:44 -0600

    user model

--- src/index.js
+++ src/index.js
@@ -9,32 +9,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-  res.send('/api/users');
-  // User.find({}, (err, result) => {
-  //   if (err) {
-  //     res.send(err);
-  //   } else {
-  //     res.send(result);
-  //   }
-  // });
+  User.find({}, (err, result) => {
+    if (err) {
+      res.json({ message: err });
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-app.post('/users', async (req, res) => {
-  res.send(req.body);
-  // const user = new User({
-  //   name: req.body.name,
-  //   username: req.body.username,
-  //   email: req.body.email,
-  //   password: bcrypt.hashSync(req.body.password, 10),
-  //   avatarUrl: imgPath,
-  // });
-  // // user.save();
-  // let error = null;
-  // try {
-  //   await user.save();
-  //   res.send(user);
-  // } catch (err) {
-  //   res.status(400).json({ message: 'ha ocurrido un error', err });
-  //   error = err;
-  // }
+app.post('api/users', (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10),
+  });
+  res.send(user);
 });
