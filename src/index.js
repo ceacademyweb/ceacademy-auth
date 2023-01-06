561a860f70fd1de6ff87dec170d546d19a04943c
Florentino Navarro <ceacademy.web@gmail.com>
2023-01-05 22:17:51 -0600

    user model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+
 const app = require('./server');
+const conn = require('./conn/conn');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  User.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
