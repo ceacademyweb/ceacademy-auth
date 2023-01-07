702da6dca9e27d1be564b5c7f2c7f59736ab824b
Florentino Navarro <ceacademy.web@gmail.com>
2023-01-07 14:51:08 -0600

    users

--- src/conn/conn.js
+++ src/conn/conn.js
@@ -3,14 +3,20 @@ const connOBJ = require('./conn.config');
 
 mongoose.set('strictQuery', false);
 const mongodb = connOBJ.mongodb;
-console.log(mongodb);
 
 //mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}
-const url3 = process.env.MONGOUSER
+const url = process.env.MONGOUSER
   ? `mongodb://${mongodb.user}:${mongodb.password}@${mongodb.host}:${mongodb.port}`
   : `mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`;
 
-console.log(`url3 = ${url3}`);
-const conn = 'hola';
+console.log(url);
+const conn = mongoose
+  .connect(url)
+  .then((db) => {
+    console.log('connection successful');
+  })
+  .catch((err) => {
+    console.log('error:', err);
+  });
 
 module.exports = conn;
