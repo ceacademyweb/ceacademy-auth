c431b95b8b9c73823219f7c485481abf3f54c12a
Florentino Navarro <ceacademy.web@gmail.com>
2023-01-17 12:05:50 -0600

    mail

---
+++ src/controllers/AuthUserController.js
@@ -0,0 +1,55 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const generarString = (min, max) => {
+  let num = Math.floor(Math.random() * (max - min + 1) + min);
+  let result = '';
+  const minus = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(
+    ' '
+  );
+  const mayus = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(
+    ' '
+  );
+  const characterSpecial = '* / $ +';
+  for (i = 0; i <= 3; i++) {
+    const random = Math.floor(Math.random() * minus.length);
+    result += minus[random];
+  }
+  for (i = 0; i <= 3; i++) {
+    const random = Math.floor(Math.random() * mayus.length);
+    result += mayus[random];
+  }
+  result += num;
+  result +=
+    characterSpecial[Math.floor(Math.random() * characterSpecial.length)];
+  return result;
+};
+
+const index = (req, res) => {
+  const random = generarString(100, 999);
+  console.log(random);
+  res.send({ random });
+};
+
+const updatePassword = (req, res) => {
+  const random = generarString(100, 999);
+  return res.send(random);
+  // const _id = req.params.id;
+  // // const data = req.body;
+  // const data = {
+  //   password: bcrypt.hashSync(random, 8),
+  //   text: random,
+  // };
+  // User.findOneAndUpdate({ _id }, data, (err, result) => {
+  //   if (err) {
+  //     res.status(401).json({ message: 'Ha ocurrido un error', err });
+  //   } else {
+  //     res.send(result);
+  //   }
+  // });
+};
+
+module.exports = {
+  index,
+  updatePassword,
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const app = require('./server');
 const conn = require('./conn/conn');
 const routesUsers = require('./routes/usersRoutes');
 const routesvideos = require('./routes/videosRoutes');
+const routesadmin = require('./routes/adminRoutes');
 const adminMiddleware = require('./middlelwares/adminMiddleware');
 
 app.get('/', (req, res) => {
@@ -9,7 +10,8 @@ app.get('/', (req, res) => {
 });
 app.use(routesUsers);
 app.use(routesvideos);
+app.use('/admin', routesadmin);
 
-app.get('/admin', adminMiddleware, (req, res) => {
-  res.send('Hello Admin!');
-});
+// app.get('/admin', adminMiddleware, (req, res) => {
+//   res.send('Hello Admin!');
+// });
