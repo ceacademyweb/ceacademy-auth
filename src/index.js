08b5e092e4221874836ca1185c7c3ee3e0cc5e08
Florentino Navarro <ceacademy.web@gmail.com>
2023-01-23 13:13:49 -0600

    login

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const conn = require('./conn/conn');
 const routesUsers = require('./routes/usersRoutes');
 const routesvideos = require('./routes/videosRoutes');
 const routesadmin = require('./routes/adminRoutes');
+const cors = require('cors');
 const adminMiddleware = require('./middlelwares/adminMiddleware');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use(cors());
 app.use(routesUsers);
 app.use(routesvideos);
 app.use('/admin', routesadmin);
